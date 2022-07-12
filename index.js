e889f653a8a118f18544f2945ab936c0fa2a2ffc
Chris Steffes <c.steffes@live.ca>
2022-07-11 21:58:30 -0500

    working highify trigger

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ var ethers = require("ethers");
 var { config } = require("./config");
 var dealerzAbi = require("./dealerz-abi.json");
 
-const url = config.providers.fantom.rpc_url;
-const ftmProvider = new ethers.providers.JsonRpcProvider(url);
+const rpc_url = config.networks.fantom.rpc_url;
+const ftmProvider = new ethers.providers.JsonRpcProvider(rpc_url);
 const nftContract = new ethers.Contract(
   config.contracts["Dealerz-Testnet"].address,
   dealerzAbi,
   ftmProvider
 );
 
-nftContract.on("Highify", async (_from, _effects, _tokenId) => {
-  console.log(_from, _effects, _tokenId);
+// const filter = {
+//   address: "0x196165FCE5b8acb582AF17E8d11887953c4a182c",
+//   topics: [ethers.utils.id("highify(uint256,uint256[])")],
+// };
+// ftmProvider.on(filter, (from, _effects, _tokenId, event) => {
+//   console.log(from);
+//   console.log(_effects);
+//   console.log(_tokenId);
+//   console.log(event.block);
+// });
+
+nftContract.on("Highify", (from, effects, tokenId, event) => {
+  console.log(from);
+  console.log(effects.toString());
+  console.log(tokenId.toString());
+  console.log(event.blockNumber);
 });
 
-console.log("Running ⚡");
+console.log("Server Running ⚡");
